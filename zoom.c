7b606a096616de74aec91e69ed8c227892ce647f
eymnhmrd <aymenhamrad@gmail.com>
2023-06-16 17:15:30 +0100

    fixed zoom function

--- zoom.c
+++ zoom.c
@@ -7,23 +7,20 @@ double  interpolate(double start, double end, double interp)
 
 void    zoom(t_fractal *f, double mouse_re, double mouse_im, double interp)
 {
-    double rapport;
-
-    rapport = 1.0 / interp;
-    f->max_x = interpolate(mouse_re, f->max_x, rapport);
-    f->max_y = interpolate(mouse_im, f->max_y, rapport);
-    f->min_x = interpolate(mouse_re, f->min_x, rapport);
-    f->max_y = interpolate(mouse_im, f->max_y, rapport);
+    f->max_x = interpolate(mouse_re, f->max_x, interp);
+    f->max_y = interpolate(mouse_im, f->max_y, interp);
+    f->min_x = interpolate(mouse_re, f->min_x, interp);
+    f->min_y = interpolate(mouse_im, f->min_y, interp);
 }
 
 int mouse_press(int button, int x, int y, t_fractal *f)
 {
     f->mouse_re = f->min_x + ((double)x / WIDTH) * (f->max_x - f->min_x);
-    f->mouse_im = f->min_y + ((double)y / WIDTH) * (f->max_y - f->min_y);
+    f->mouse_im = f->min_y + ((double)y / HEIGHT) * (f->max_y - f->min_y);
     if (button == 1)
-        zoom(f, f->mouse_re, f->mouse_im, 2);
-    else if (button == 3)
         zoom(f, f->mouse_re, f->mouse_im, 0.5);
+    else if (button == 3)
+        zoom(f, f->mouse_re, f->mouse_im, 2);
     render(f);
     return (0);
 }
